4a9421021505b2d15994c303bfde782932f9f098
Rida Sattar <rida_sattar@live.com>
2024-02-23 11:49:10 +1100

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'cypress';
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      baseUrl: 'https://parabank.parasoft.com';
-      defaultCommandTimeout: 10000;
-    },
+    baseUrl: 'https://parabank.parasoft.com',
+    defaultCommandTimeout: 10000,
   },
 });
